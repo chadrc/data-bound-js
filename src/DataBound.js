fa61e3e8b78d15fc32215354794016c21e1e8f28
chadrc <chadrcollins91@gmail.com>
2016-11-16 21:05:24 -0500

    Removed redundant trim

--- src/DataBound.js
+++ src/DataBound.js
@@ -3,7 +3,7 @@ class DataBoundUtils {
     static extractPropFromString(propStr) {
         let match = propStr.match(DataBoundUtils.propStringRegex);
         if (match && match.length > 0) {
-            return match[0].trim().slice(2, -1).trim();
+            return match[0].slice(2, -1).trim();
         }
         return "";
     }
