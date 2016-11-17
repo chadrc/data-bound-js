6aca650a09df6d86a64f8fbff0f357f49adc9524
chadrc <chadrcollins91@gmail.com>
2016-11-16 21:27:00 -0500

    Made prop extracting return multiple values

--- src/DataBound.js
+++ src/DataBound.js
@@ -1,9 +1,13 @@
 
 class DataBoundUtils {
-    static extractPropFromString(propStr) {
+    static extractPropsFromString(propStr) {
         let match = propStr.match(DataBoundUtils.propStringRegex);
         if (match && match.length > 0) {
-            return {match: match[0], prop: match[0].slice(2, -1).trim()};
+            let props = [];
+            for (let i=0; i<match.length; i++) {
+                props.push({match: match[i], prop: match[i].slice(2, -1).trim()})
+            }
+            return props;
         }
         return null;
     }
--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,6 @@
 
 TestSuites.Utils = {
-    extractPropFromString: function test_extractPropFromString() {
+    extractPropsFromString: function test_extractPropsFromString() {
         const propsToTest = [
             {prop: "${myProp}", expected: {expectedProp: "myProp", expectedMatch: "${myProp}"}}, // 0
             {prop: "  ${myProp}  ", expected: {expectedProp: "myProp", expectedMatch: "${myProp}"}}, // 1
@@ -15,15 +15,16 @@ TestSuites.Utils = {
             {prop: "${.~myProp}", expected: null}, // 10
             {prop: "not props ${aProp}", expected: {expectedProp: "aProp", expectedMatch: "${aProp}"}}, // 11
             {prop: "${no spaces}", expected: null}, // 12
-            {prop: "${seci@lCh@r@cters", expected: null} // 13
+            {prop: "${seci@lCh@r@cters}", expected: null} // 13
         ];
 
         for (let i = 0; i < propsToTest.length; i++) {
             let pair = propsToTest[i];
-            let dbProp = DataBoundUtils.extractPropFromString(pair.prop);
+            let dbProp = DataBoundUtils.extractPropsFromString(pair.prop);
             if (pair.expected == null) {
                 assert(dbProp == null, i + ": Expecting no match, got ", dbProp);
             } else {
+                dbProp = dbProp[0];
                 assert(dbProp.prop == pair.expected.expectedProp, i + ": Extracted prop should be '" + pair.expected.expectedProp + "', got '" + dbProp.prop + "'");
                 assert(dbProp.match == pair.expected.expectedMatch, i + ": Extracted match should be '" + pair.expected.expectedMatch + "', got '" + dbProp.match + "'");
             }
