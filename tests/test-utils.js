4a79dffff46b2dc5249bbe9d72f9f648f66bbc36
chadrc <chadrcollins91@gmail.com>
2016-11-17 22:35:20 -0500

    Re-worked testing layout and workflow, created interface for it

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -8,25 +8,139 @@ function assert(condition, failMessage) {
     }
 }
 
-let TestSuites = {};
+function runTestMethod(suiteNo, testNo) {
+
+}
+
+function createTestDisplay() {
+    let root = document.createElement('div');
+    let header = document.createElement('h1');
+    header.innerHTML = TestSuites.applicationName;
+    root.appendChild(header);
+
+    for (let i=0; i<TestSuites.suites.length; i++) {
+        let suite = TestSuites.suites[i];
+        let suiteRoot = document.createElement('div');
+        suiteRoot.setAttribute('class', 'card');
+
+        let suiteHeader = document.createElement('div');
+        suiteHeader.setAttribute('class', 'card-header');
+
+        let suiteRunBtn = document.createElement('button');
+        suiteRunBtn.setAttribute('type', 'button');
+        suiteRunBtn.setAttribute('class', 'btn btn-secondary mr-1');
+        suiteRunBtn.innerHTML = "Run";
+
+        let suiteName = document.createElement('a');
+        suiteName.setAttribute('class', 'h2 align-middle');
+        suiteName.innerHTML = suite.name;
+
+        suiteName.setAttribute('data-toggle', 'collapse');
+        let suiteToggleId = "Suite" + i + "CollapseToggle";
+        suiteName.setAttribute('href', "#" + suiteToggleId);
+        suiteName.setAttribute('aria-expanded', 'false');
+        suiteName.setAttribute('aria-controls', suiteToggleId);
+
+        suiteHeader.appendChild(suiteRunBtn);
+        suiteHeader.appendChild(suiteName);
+
+        suiteRoot.appendChild(suiteHeader);
+
+        let suiteTestContainer = document.createElement('div');
+        suiteTestContainer.setAttribute('class', 'collapse');
+        suiteTestContainer.setAttribute('id', suiteToggleId);
+
+        for (let j=0; j<suite.tests.length; j++) {
+            let test = suite.tests[j];
+
+            let testRoot = document.createElement('div');
+            testRoot.setAttribute('class', 'card-block m-1 rounded card-inverse card-info');
+
+            let testHeader = document.createElement('h5');
+            testHeader.setAttribute('class', 'card-title');
+
+            let testRunBtn = document.createElement('button');
+            testRunBtn.setAttribute('type', 'button');
+            testRunBtn.setAttribute('class', 'btn btn-secondary btn-sm mr-1');
+            testRunBtn.innerHTML = "Run";
+
+            let testName = document.createElement('span');
+            testName.setAttribute('class', 'align-middle');
+            testName.innerHTML = test.name;
+
+            testHeader.appendChild(testRunBtn);
+            testHeader.appendChild(testName);
+
+            let testMessage = document.createElement('p');
+            testMessage.setAttribute('class', 'card-text');
+            testMessage.innerHTML = "Not Run";
+
+            testRoot.appendChild(testHeader);
+            testRoot.appendChild(testMessage);
+
+            suiteTestContainer.appendChild(testRoot);
+        }
+
+        suiteRoot.appendChild(suiteTestContainer);
+
+        root.appendChild(suiteRoot);
+    }
+
+    document.body.appendChild(root);
+}
+
+let TestSuites = {
+    applicationName: "Test Application",
+    suites: []
+};
 
 window.addEventListener('load', function () {
-    let suites = Object.keys(TestSuites);
-    for (let i=0; i<suites.length; i++) {
-        let suiteName = suites[i];
-        console.log("Starting Suite: " + suiteName);
-        let suite = TestSuites[suiteName];
-        let tests = Object.keys(suite);
-        for (let j=0; j<tests.length; j++) {
-            let testName = tests[j];
-            console.log("testing: ", testName);
-            let test = suite[testName];
-            try {
-                test();
-                console.log("success");
-            } catch (err) {
-                console.error(err);
-            }
-        }
-    }
+    createTestDisplay();
+
+    // let suites = Object.keys(TestSuites);
+    // for (let i=0; i<suites.length; i++) {
+    //     let suiteName = suites[i];
+    //     console.log("Starting Suite: " + suiteName);
+    //     let suite = TestSuites[suiteName];
+    //     let tests = Object.keys(suite);
+    //     for (let j=0; j<tests.length; j++) {
+    //         let testName = tests[j];
+    //         console.log("testing: ", testName);
+    //         let test = suite[testName];
+    //         try {
+    //             test();
+    //             console.log("success");
+    //         } catch (err) {
+    //             console.error(err);
+    //         }
+    //     }
+    // }
 });
+
+/**
+ <div>
+     <h1>Application Name</h1>
+     <div class="card">
+         <div class="card-header">
+             <button type="button" class="btn btn-secondary">Run</button>
+
+             <span class="h2 align-middle">Suite Name</span>
+
+             <button type="button" class="btn btn-secondary btn-sm"
+             data-toggle="collapse"
+             data-target="#suiteCollapse"
+             aria-expanded="false"
+             aria-controls="suiteCollapse">Toggle Tests</button>
+         </div>
+         <div class="collapse" id="suiteCollapse">
+             <div class="card-block card-inverse card-danger m-1 rounded">
+                 <h5 class="card-title">
+                     <button type="button" class="btn btn-secondary btn-sm">Run</button>
+                     <span class="align-middle">Test Name</span>
+                 </h5>
+                 <p class="card-text">Status Message</p>
+             </div>
+        </div>
+     </div>
+ </div>
+ **/
--- tests/tests.js
+++ tests/tests.js
@@ -1,111 +1,135 @@
 
-TestSuites.Utils = {
-    extractSingleProp: () => {
-        const cases = [
-            {prop: "${myProp}",
-                expected: {expectedProp: "myProp", expectedMatch: "${myProp}"}}, // 0
-            {prop: "  ${myProp}  ",
-                expected: {expectedProp: "myProp", expectedMatch: "${myProp}"}}, // 1
-            {prop: "${  myProp  }",
-                expected: {expectedProp: "myProp", expectedMatch: "${  myProp  }"}}, // 2
-            {prop: "${~rootProp}",
-                expected: {expectedProp: "rootProp", expectedMatch: "${~rootProp}", expectedRoot: true}}, // 3
-            {prop: "${root~Prop}", expected: null}, // 4
-            {prop: "${rootProp~}", expected: null}, // 5
-            {prop: "${myProp.subProp}",
-                expected: {expectedProp: "myProp.subProp", expectedMatch: "${myProp.subProp}"}}, // 6
-            {prop: "${.myProp.subProp}",
-                expected: {expectedProp: "myProp.subProp", expectedMatch: "${.myProp.subProp}", expectedSelf: true}}, // 7
-            {prop: "${myProp.}", expected: null}, // 8
-            {prop: "${~.myProp}", expected: null}, // 9
-            {prop: "${.~myProp}", expected: null}, // 10
-            {prop: "not props ${aProp}",
-                expected: {expectedProp: "aProp", expectedMatch: "${aProp}"}}, // 11
-            {prop: "${no spaces}", expected: null}, // 12
-            {prop: "${seci@lCh@r@cters}", expected: null} // 13
-        ];
+TestSuites.suites.push({
+    name: "Utils",
+    tests: [
+        {
+            name: "Extract Single Prop",
+            method: () => {
+                const cases = [
+                    {prop: "${myProp}",
+                        expected: {expectedProp: "myProp", expectedMatch: "${myProp}"}}, // 0
+                    {prop: "  ${myProp}  ",
+                        expected: {expectedProp: "myProp", expectedMatch: "${myProp}"}}, // 1
+                    {prop: "${  myProp  }",
+                        expected: {expectedProp: "myProp", expectedMatch: "${  myProp  }"}}, // 2
+                    {prop: "${~rootProp}",
+                        expected: {expectedProp: "rootProp", expectedMatch: "${~rootProp}", expectedRoot: true}}, // 3
+                    {prop: "${root~Prop}", expected: null}, // 4
+                    {prop: "${rootProp~}", expected: null}, // 5
+                    {prop: "${myProp.subProp}",
+                        expected: {expectedProp: "myProp.subProp", expectedMatch: "${myProp.subProp}"}}, // 6
+                    {prop: "${.myProp.subProp}",
+                        expected: {expectedProp: "myProp.subProp", expectedMatch: "${.myProp.subProp}", expectedSelf: true}}, // 7
+                    {prop: "${myProp.}", expected: null}, // 8
+                    {prop: "${~.myProp}", expected: null}, // 9
+                    {prop: "${.~myProp}", expected: null}, // 10
+                    {prop: "not props ${aProp}",
+                        expected: {expectedProp: "aProp", expectedMatch: "${aProp}"}}, // 11
+                    {prop: "${no spaces}", expected: null}, // 12
+                    {prop: "${seci@lCh@r@cters}", expected: null} // 13
+                ];
 
-        for (let i = 0; i < cases.length; i++) {
-            let pair = cases[i];
-            let props = DataBoundUtils.extractPropsFromString(pair.prop);
-            if (pair.expected == null) {
-                assert(props.length == 0, i + ": Expecting no match, got ", props);
-            } else {
-                props = props[0];
-                if (pair.expectedRoot) {
-                    assert(props.rootRef == true, i + ": Expected to be root ref.");
+                for (let i = 0; i < cases.length; i++) {
+                    let pair = cases[i];
+                    let props = DataBoundUtils.extractPropsFromString(pair.prop);
+                    if (pair.expected == null) {
+                        assert(props.length == 0, i + ": Expecting no match, got ", props);
+                    } else {
+                        props = props[0];
+                        if (pair.expectedRoot) {
+                            assert(props.rootRef == true, i + ": Expected to be root ref.");
+                        }
+                        if (pair.expectedSelf) {
+                            assert(props.selfRef == true, i + ": Expected to be self ref.");
+                        }
+                        assert(props.prop == pair.expected.expectedProp, i + ": Extracted prop should be '" +
+                            pair.expected.expectedProp + "', got '" + props.prop + "'");
+                        assert(props.match == pair.expected.expectedMatch, i + ": Extracted match should be '" +
+                            pair.expected.expectedMatch + "', got '" + props.match + "'");
+                    }
                 }
-                if (pair.expectedSelf) {
-                    assert(props.selfRef == true, i + ": Expected to be self ref.");
+            },
+        },
+        {
+            name: "Extract Multiple Props",
+            method: () => {
+                let cases = [
+                    {str: "no props at all", expectedCount: 0},
+                    {str: "${onlyProp}", expectedCount: 1,
+                        expectedMatches: [{prop: "onlyProp", match: "${onlyProp}"}]},
+                    {str: "${firstProp} some middle text ${secondProp}", expectedCount: 2,
+                        expectedMatches: [{prop: "firstProp", match: "${firstProp}"},
+                            {prop: "secondProp", match: "${secondProp}"}]},
+                    {str: "${firstProp ${secondProp}}", expectedCount: 1,
+                        expectedMatches: [{prop: "secondProp", match: "${secondProp}"}]},
+                    {str: "${firstProp ${secondProp}", expectedCount: 1,
+                        expectedMatches: [{prop: "secondProp", match: "${secondProp}"}]},
+                ];
+
+                for (let i = 0; i < cases.length; i++) {
+                    let pair = cases[i];
+                    let props = DataBoundUtils.extractPropsFromString(pair.str);
+                    assert(props.length == pair.expectedCount,
+                        i + ": Expected " + pair.expectedCount + " props, got " + props.length);
+                    if (pair.expectedCount > 0) {
+                        for (let j=0; j<pair.expectedCount; j++) {
+                            assert(pair.expectedMatches[j].prop == props[j].prop
+                                && pair.expectedMatches[j].match == props[j].match,
+                                j + ": Expected ", pair.expectedMatches[j], + ", got ", props[j]);
+                        }
+                    }
                 }
-                assert(props.prop == pair.expected.expectedProp, i + ": Extracted prop should be '" +
-                    pair.expected.expectedProp + "', got '" + props.prop + "'");
-                assert(props.match == pair.expected.expectedMatch, i + ": Extracted match should be '" +
-                    pair.expected.expectedMatch + "', got '" + props.match + "'");
+            },
+        }
+    ]
+});
+
+TestSuites.suites.push({
+    name: "Bound Prop String",
+    tests: [
+        {
+            name: "Single Prop String Render",
+            method: () => {
+                        let testStr = "Hello, World!";
+                        let propStr = new DataBoundPropString("${myProp}");
+                        let renderStr = propStr.renderWithContext({myProp: testStr});
+                        assert(renderStr == testStr, "Expected " + testStr + ", got " + renderStr);
+                    }
+        },
+        {
+            name: "Multi Prop String Render",
+            method: () => {
+                let testStr = "Hello, World!";
+                let propStr = new DataBoundPropString("${firstProp} ${secondProp}");
+                let renderStr = propStr.renderWithContext({firstProp: "Hello,", secondProp: "World!"});
+                assert(renderStr == testStr, "Expected " + testStr + ", got " + renderStr);
+            }
+        },
+        {
+            name: "Self Prop String Render",
+            method: () => {
+                let testStr = "Hello, Self!";
+                let propStr = new DataBoundPropString("${.helloProp}");
+                let renderStr = propStr.renderWithContext(
+                    {helloProp: "Goodbye"},
+                    {helloProp: testStr},
+                    {helloProp: "Hello, World!"}
+                );
+                assert(renderStr == testStr, "Expected " + testStr + ", got " + renderStr);
+            }
+        },
+        {
+            name: "Root Prop String Render",
+            method: () => {
+                let testStr = "Hello, World!";
+                let propStr = new DataBoundPropString("${~helloProp}");
+                let renderStr = propStr.renderWithContext(
+                    {helloProp: "Goodbye"},
+                    {helloProp: "Hello, Self!"},
+                    {helloProp: testStr}
+                );
+                assert(renderStr == testStr, "Expected " + testStr + ", got " + renderStr);
             }
         }
-    },
-    extractMultipleProps: () => {
-        let cases = [
-            {str: "no props at all", expectedCount: 0},
-            {str: "${onlyProp}", expectedCount: 1,
-                expectedMatches: [{prop: "onlyProp", match: "${onlyProp}"}]},
-            {str: "${firstProp} some middle text ${secondProp}", expectedCount: 2,
-                expectedMatches: [{prop: "firstProp", match: "${firstProp}"},
-                    {prop: "secondProp", match: "${secondProp}"}]},
-            {str: "${firstProp ${secondProp}}", expectedCount: 1,
-                expectedMatches: [{prop: "secondProp", match: "${secondProp}"}]},
-            {str: "${firstProp ${secondProp}", expectedCount: 1,
-                expectedMatches: [{prop: "secondProp", match: "${secondProp}"}]},
-        ];
-
-        for (let i = 0; i < cases.length; i++) {
-            let pair = cases[i];
-            let props = DataBoundUtils.extractPropsFromString(pair.str);
-            assert(props.length == pair.expectedCount,
-                i + ": Expected " + pair.expectedCount + " props, got " + props.length);
-            if (pair.expectedCount > 0) {
-                for (let j=0; j<pair.expectedCount; j++) {
-                    assert(pair.expectedMatches[j].prop == props[j].prop
-                        && pair.expectedMatches[j].match == props[j].match,
-                    j + ": Expected ", pair.expectedMatches[j], + ", got ", props[j]);
-                }
-            }
-        }
-    }
-};
-
-TestSuites.BoundPropString = {
-    singlePropStringRender: () => {
-        let testStr = "Hello, World!";
-        let propStr = new DataBoundPropString("${myProp}");
-        let renderStr = propStr.renderWithContext({myProp: testStr});
-        assert(renderStr == testStr, "Expected " + testStr + ", got " + renderStr);
-    },
-    multiPropStringRender: () => {
-        let testStr = "Hello, World!";
-        let propStr = new DataBoundPropString("${firstProp} ${secondProp}");
-        let renderStr = propStr.renderWithContext({firstProp: "Hello,", secondProp: "World!"});
-        assert(renderStr == testStr, "Expected " + testStr + ", got " + renderStr);
-    },
-    selfPropStringRender: () => {
-        let testStr = "Hello, Self!";
-        let propStr = new DataBoundPropString("${.helloProp}");
-        let renderStr = propStr.renderWithContext(
-            {helloProp: "Goodbye"},
-            {helloProp: testStr},
-            {helloProp: "Hello, World!"}
-        );
-        assert(renderStr == testStr, "Expected " + testStr + ", got " + renderStr);
-    },
-    rootPropStringRender: () => {
-        let testStr = "Hello, World!";
-        let propStr = new DataBoundPropString("${~helloProp}");
-        let renderStr = propStr.renderWithContext(
-            {helloProp: "Goodbye"},
-            {helloProp: "Hello, Self!"},
-            {helloProp: testStr}
-        );
-        assert(renderStr == testStr, "Expected " + testStr + ", got " + renderStr);
-    }
-};
+    ]
+});
