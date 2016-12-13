bab4588d5fc87c7077fb2d12eb6434a3f854f9eb
chadrc <chadrcollins91@gmail.com>
2016-12-12 23:02:32 -0500

    Created data object for projects page.

---
+++ samples/simple-site/data/projects-data.js
@@ -0,0 +1,100 @@
+/**
+ * Created by chad on 12/12/16.
+ */
+
+let ProjectsData = {
+    projects: [
+        {
+            name: "Websites",
+            image: "blue-square.png",
+            article:
+                `<p>
+                    Pellentesque nec urna consectetur mi suscipit eleifend. Phasellus viverra magna risus, dapibus
+                    suscipit velit rhoncus ut. Pellentesque fermentum viverra sagittis. Integer non consequat elit, 
+                    sed congue ligula. Nullam quis ligula ut enim egestas finibus id sed elit. Nam commodo dictum 
+                    pulvinar. Vivamus eget turpis eu quam volutpat scelerisque. Mauris ut iaculis odio. Donec eget 
+                    volutpat enim. Maecenas interdum ornare mauris, vitae tincidunt elit fermentum eget. Vestibulum 
+                    elit leo, cursus eget ligula ut, feugiat eleifend odio. Sed eu eros sed risus vulputate ultrices 
+                    quis id lectus. Integer feugiat finibus enim ut hendrerit. Vivamus scelerisque lacus at commodo 
+                    porttitor. Donec enim velit, lobortis vitae mauris ut, rhoncus malesuada turpis.
+                    <img class="float-xs-left" src="images/blue-square.png" width="300px" height="300px">
+                </p>`
+        },
+        {
+            name: "Web Services",
+            image: "blue-square.png",
+            article:
+                `<p>
+                    Ut imperdiet quam enim, sed iaculis ex tincidunt eu. Maecenas dictum tempus massa at commodo. 
+                    Praesent mollis ante ligula, ac egestas libero laoreet nec. Ut a mauris ut purus maximus rhoncus. 
+                    Donec sollicitudin ultricies varius. Sed sed imperdiet turpis. Nullam tortor ligula, ornare sed 
+                    commodo vel, aliquet in massa. Ut ut magna venenatis, varius risus a, rutrum neque. Pellentesque 
+                    ultricies risus accumsan erat venenatis eleifend. In porta, mi sit amet cursus ornare, metus sem 
+                    posuere elit, a tincidunt diam erat nec justo. Cras malesuada elementum tortor, ac efficitur urna 
+                    tempus a. Sed id dolor a ipsum laoreet tincidunt sed consectetur leo. Phasellus non nunc quis nibh
+                    bibendum auctor. Cras pellentesque dapibus est, sit amet scelerisque dolor volutpat vel.
+                    <img class="float-xs-right" src="images/blue-square.png" width="400px" height="200px">
+                </p>`
+        },
+        {
+            name: "Cloud Architecture",
+            image: "blue-square.png",
+            article:
+                `<p>
+                    <img class="float-xs-right" src="images/blue-square.png" width="200px" height="300px">
+                    Donec efficitur in nibh at aliquet. Vestibulum leo justo, egestas nec laoreet et, tristique 
+                    id sem. Aliquam eu viverra lacus, a placerat arcu. Phasellus faucibus mi dictum eros rhoncus pharetra 
+                    quis id diam. Phasellus porta porta diam id semper. Ut dictum turpis fermentum enim dapibus faucibus. 
+                    Aliquam sed ullamcorper ipsum. Mauris eu facilisis libero. Morbi ultricies ex in leo scelerisque, id 
+                    pharetra urna vulputate. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per 
+                    inceptos himenaeos. Cras volutpat erat ligula, quis ultrices sem dapibus ut. Donec ut porttitor 
+                    sapien. Morbi id placerat metus, non condimentum massa. Vivamus justo augue, tempor ac nibh et, 
+                    maximus rhoncus ante. Nulla congue fringilla turpis eu semper. Suspendisse tincidunt quam turpis, 
+                    non cursus risus porttitor at.
+                </p>`
+        },
+        {
+            name: "Mobile Games",
+            image: "blue-square.png",
+            article:
+                `<p>
+                    Morbi suscipit scelerisque metus. Quisque sed aliquam orci. Donec sit amet vulputate quam, 
+                    ut efficitur dolor. Ut consectetur metus vel urna imperdiet laoreet. Maecenas pulvinar molestie 
+                    mauris, sit amet fermentum nisl cursus eu. Curabitur luctus quam quis commodo molestie. Nam nisi 
+                    neque, dictum id aliquet eget, pharetra interdum dolor. Aenean maximus felis magna, tempus 
+                    ultrices dolor dignissim quis.
+                </p>
+                <p>
+                    <img class="float-xs-right" src="images/blue-square.png" width="250px" height="250px">
+                    Etiam congue aliquam laoreet. Aenean eu mi at neque sagittis porta. Mauris et augue non libero 
+                    pharetra vestibulum. Vivamus a ultricies ante. Suspendisse pretium mauris justo, eget ullamcorper
+                    leo sollicitudin eget. Integer eu tincidunt risus, eget blandit dui. Aenean non aliquet magna. 
+                    Cras condimentum elementum mauris, ac lacinia lectus mattis id. Fusce non finibus arcu, eu 
+                    laoreet tortor.
+                </p>`
+        },
+        {
+            name: "UX Design",
+            image: "blue-square.png",
+            article:
+                `<p>
+                    Nullam tincidunt lacinia sodales. Donec luctus ac nibh quis sagittis. Vestibulum purus mauris, 
+                    ultrices eget vehicula nec, maximus in justo. Etiam vel sem laoreet, suscipit metus nec, 
+                    ullamcorper ante. Phasellus in lacus odio. Suspendisse tortor magna, commodo sed massa vitae, 
+                    dignissim dictum tellus. Morbi ullamcorper tincidunt lectus, fringilla pharetra nunc volutpat ut. 
+                    Ut molestie, sem at porttitor mattis, tortor nibh consequat odio, at sollicitudin lorem neque nec
+                    purus. Duis ac ipsum sem.
+                    <img class="float-xs-left" src="images/blue-square.png" width="150px" height="300px">
+                </p>
+                <p>
+                    <img class="float-xs-right" src="images/blue-square.png" width="400px" height="250px">
+                    Donec facilisis pulvinar sem, eu sollicitudin risus. Suspendisse porttitor, sapien eget finibus 
+                    vehicula, metus diam convallis erat, nec dictum erat arcu eu erat. Donec hendrerit nulla nunc, 
+                    vel ullamcorper enim tincidunt non. Morbi suscipit vel leo at pellentesque. Vivamus eget erat a 
+                    magna tristique placerat. Nam ipsum magna, malesuada vitae vestibulum non, aliquet quis augue. 
+                    Aenean hendrerit venenatis eros, ac facilisis sapien aliquam et. Suspendisse potenti. In hac 
+                    habitasse platea dictumst. Praesent dapibus rhoncus ante posuere vestibulum.
+                </p>`
+        },
+    ]
+};
