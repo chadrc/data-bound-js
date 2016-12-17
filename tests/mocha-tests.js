6c1bc875e9d7a438c95ef5052842de3685a7e266
chadrc <chadrcollins91@gmail.com>
2016-12-17 12:37:58 -0500

    Transfered over a few more tests for prop extraction. Restructured suite to be dynamic.

--- tests/mocha-tests.js
+++ tests/mocha-tests.js
@@ -2,25 +2,48 @@
  * Created by chad on 12/16/16.
  */
 
-describe('Prop Parsing', function() {
-    describe('${myProp}', function() {
+describe('Prop Parsing', function () {
 
-        var result = DataBoundUtils.extractPropsFromString("${myProp}");
+    var tests = [
+      {
+        propStr: "${myProp}", expectedLength: 1,
+        expectedValues: [{expectedProp: "myProp", expectedMatch: "${myProp}"}]
+      },
+      {
+        propStr: "    ${myProp}    ", expectedLength: 1,
+        expectedValues: [{expectedProp: "myProp", expectedMatch: "${myProp}"}]
+      },
+      {
+        propStr: "${    myProp    }", expectedLength: 1,
+        expectedValues: [{expectedProp: "myProp", expectedMatch: "${    myProp    }"}]
+      },
+      {
+        propStr: "${seci@lCh@r@cters}", expectedLength: 0, expectedValues: []
+      },
+      {
+        propStr: "${no spaces}", expectedLength: 0, expectedValues: []
+      },
+    ];
 
-        it('should return ok value', function() {
-            expect(result).to.be.ok;
+    tests.forEach(function (test) {
+      describe("'" + test.propStr + "'", function () {
+      var result = DataBoundUtils.extractPropsFromString(test.propStr);
+
+      it('result should have ' + test.expectedLength + ' value' + (test.expectedLength == 1 ? "" : "s"), function () {
+        expect(result).to.have.length(test.expectedLength);
+      });
+
+      for (var i = 0; i < result.length; i++) {
+        var value = result[i];
+        var expectedValue = test.expectedValues[i];
+        it('value ' + (i+1) + ' should have field "prop" with "' + expectedValue.expectedProp + '" as value', function () {
+          expect(value.prop).to.deep.equal(expectedValue.expectedProp);
         });
 
-        it('should have 1 value', function () {
-            expect(result).to.have.length(1) ;
+        it('value ' + (i+1)  + ' should have field "match" with "' + expectedValue.expectedMatch + '" as value', function () {
+          expect(value.match).to.deep.equal(expectedValue.expectedMatch);
         });
-
-        it('should have field "prop" with "myProp" as value', function () {
-            expect(result[0].prop).to.deep.equal("myProp");
-        });
-
-        it('should have field "match" with "${myProp}" as value', function () {
-            expect(result[0].match).to.deep.equal("${myProp}");
-        });
+      }
     });
+  });
 });
