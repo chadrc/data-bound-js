86ab62620adc82624fd299f3c61d235f3d4509c0
chadrc <chadrcollins91@gmail.com>
2016-12-16 23:58:51 -0500

    Added first sweet for prop parsing.

--- tests/mocha-tests.js
+++ tests/mocha-tests.js
@@ -2,10 +2,25 @@
  * Created by chad on 12/16/16.
  */
 
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1,2,3].indexOf(4));
+describe('Prop Parsing', function() {
+    describe('${myProp}', function() {
+
+        var result = DataBoundUtils.extractPropsFromString("${myProp}");
+
+        it('should return ok value', function() {
+            expect(result).to.be.ok;
+        });
+
+        it('should have 1 value', function () {
+            expect(result).to.have.length(1) ;
+        });
+
+        it('should have field "prop" with "myProp" as value', function () {
+            expect(result[0].prop).to.deep.equal("myProp");
+        });
+
+        it('should have field "match" with "${myProp}" as value', function () {
+            expect(result[0].match).to.deep.equal("${myProp}");
         });
     });
 });
