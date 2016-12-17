94c4f4ba92f5f24de7dca8abaf0e88d14ec9982f
chadrc <chadrcollins91@gmail.com>
2016-12-17 13:32:51 -0500

    Finished transfering prop parsing tests over to mocha.

--- tests/mocha-tests.js
+++ tests/mocha-tests.js
@@ -4,45 +4,167 @@
 
 describe('Prop Parsing', function () {
 
-    var tests = [
-      {
-        propStr: "${myProp}", expectedLength: 1,
-        expectedValues: [{expectedProp: "myProp", expectedMatch: "${myProp}"}]
-      },
-      {
-        propStr: "    ${myProp}    ", expectedLength: 1,
-        expectedValues: [{expectedProp: "myProp", expectedMatch: "${myProp}"}]
-      },
-      {
-        propStr: "${    myProp    }", expectedLength: 1,
-        expectedValues: [{expectedProp: "myProp", expectedMatch: "${    myProp    }"}]
-      },
-      {
-        propStr: "${seci@lCh@r@cters}", expectedLength: 0, expectedValues: []
-      },
-      {
-        propStr: "${no spaces}", expectedLength: 0, expectedValues: []
-      },
-    ];
+  var tests = [
+    {
+      propStr: "${myProp}", expectedLength: 1,
+      expectedValues: [
+        {
+          expectedProp: "myProp", expectedMatch: "${myProp}",
+          expectedRootRef: false, expectedBoundRef: false
+        }]
+    },
+    {
+      propStr: "    ${myProp}    ", expectedLength: 1,
+      expectedValues: [
+        {
+          expectedProp: "myProp", expectedMatch: "${myProp}",
+          expectedRootRef: false, expectedBoundRef: false,
+        }]
+    },
+    {
+      propStr: "${    myProp    }", expectedLength: 1,
+      expectedValues: [
+        {
+          expectedProp: "myProp", expectedMatch: "${    myProp    }",
+          expectedRootRef: false, expectedBoundRef: false
+        }]
+    },
+    {
+      propStr: "${seci@lCh@r@cters}", expectedLength: 0,
+      expectedValues: []
+    },
+    {
+      propStr: "${no spaces}", expectedLength: 0,
+      expectedValues: []
+    },
+    {
+      propStr: "notAProp", expectedLength: 0,
+      expectedValues: []
+    },
+    {
+      propStr: "not props ${aProp}", expectedLength: 1,
+      expectedValues: [
+        {
+          expectedProp: "aProp", expectedMatch: "${aProp}",
+          expectedRootRef: false, expectedBoundRef: false
+        }]
+    },
+    {
+      propStr: "${firstProp} some middle text ${secondProp}", expectedLength: 2,
+      expectedValues: [
+        {
+          expectedProp: "firstProp", expectedMatch: "${firstProp}",
+          expectedRootRef: false, expectedBoundRef: false
+        },
+        {
+          expectedProp: "secondProp", expectedMatch: "${secondProp}",
+          expectedRootRef: false, expectedBoundRef: false
+        }
+      ]
+    },
+    {
+      propStr: "${firstProp ${secondProp}}", expectedLength: 1,
+      expectedValues: [
+        {
+          expectedProp: "secondProp", expectedMatch: "${secondProp}",
+          expectedRootRef: false, expectedBoundRef: false
+        }]
+    },
+    {
+      propStr: "${firstProp ${secondProp}", expectedLength: 1,
+      expectedValues: [
+        {
+          expectedProp: "secondProp", expectedMatch: "${secondProp}",
+          expectedRootRef: false, expectedBoundRef: false
+        }]
+    },
+    {
+      propStr: "${~rootProp}", expectedLength: 1,
+      expectedValues: [
+        {
+          expectedProp: "rootProp", expectedMatch: "${~rootProp}",
+          expectedRootRef: true, expectedBoundRef: false
+        }]
+    },
+    {
+      propStr: "${root~Prop}", expectedLength: 0,
+      expectedValues: []
+    },
+    {
+      propStr: "${rootProp~}", expectedLength: 0,
+      expectedValues: []
+    },
+    {
+      propStr: "${.myProp}", expectedLength: 1,
+      expectedValues: [
+        {
+          expectedProp: "myProp", expectedMatch: "${.myProp}",
+          expectedRootRef: false, expectedBoundRef: true
+        }]
+    },
+    {
+      propStr: "${myProp.}", expectedLength: 0,
+      expectedValues: []
+    },
+    {
+      propStr: "${~.myProp}", expectedLength: 0,
+      expectedValues: []
+    },
+    {
+      propStr: "${.~myProp}", expectedLength: 0,
+      expectedValues: []
+    },
+    {
+      propStr: "${myProp.subProp}", expectedLength: 1,
+      expectedValues: [
+        {
+          expectedProp: "myProp.subProp", expectedMatch: "${myProp.subProp}",
+          expectedRootRef: false, expectedBoundRef: false
+        }]
+    },
+    {
+      propStr: "${.myProp.subProp}", expectedLength: 1,
+      expectedValues: [
+        {
+          expectedProp: "myProp.subProp", expectedMatch: "${.myProp.subProp}",
+          expectedRootRef: false, expectedBoundRef: true
+        }]
+    },
+  ];
 
-    tests.forEach(function (test) {
-      describe("'" + test.propStr + "'", function () {
+  tests.forEach(function (test) {
+    describe("'" + test.propStr + "'", function () {
       var result = DataBoundUtils.extractPropsFromString(test.propStr);
 
-      it('result should have ' + test.expectedLength + ' value' + (test.expectedLength == 1 ? "" : "s"), function () {
-        expect(result).to.have.length(test.expectedLength);
-      });
+      it('result should have ' + test.expectedLength + ' prop' + (test.expectedLength == 1 ? "" : "s"),
+        function () {
+          expect(result).to.have.length(test.expectedLength);
+        });
 
       for (var i = 0; i < result.length; i++) {
         var value = result[i];
         var expectedValue = test.expectedValues[i];
-        it('value ' + (i+1) + ' should have field "prop" with "' + expectedValue.expectedProp + '" as value', function () {
-          expect(value.prop).to.deep.equal(expectedValue.expectedProp);
-        });
+        it('prop ' + (i + 1) + ' should have field "prop" with "' + expectedValue.expectedProp + '" as value',
+          function () {
+            expect(value.prop).to.deep.equal(expectedValue.expectedProp);
+          });
 
-        it('value ' + (i+1)  + ' should have field "match" with "' + expectedValue.expectedMatch + '" as value', function () {
-          expect(value.match).to.deep.equal(expectedValue.expectedMatch);
-        });
+        it('prop ' + (i + 1) + ' should have field "match" with "' + expectedValue.expectedMatch + '" as value',
+          function () {
+            expect(value.match).to.deep.equal(expectedValue.expectedMatch);
+          });
+
+        if (expectedValue.expectedBoundRef) {
+          it('prop ' + (i + 1) + ' should have data bound reference', function () {
+            expect(value.selfRef).to.be.true;
+          })
+        }
+
+        if (expectedValue.expectedRootRef) {
+          it('prop ' + (i + 1) + ' should have root reference', function () {
+            expect(value.rootRef).to.be.true;
+          })
+        }
       }
     });
   });
