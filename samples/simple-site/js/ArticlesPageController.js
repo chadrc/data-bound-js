468c866812109cadc6f669c5ed6dd3edc162c06a
chadrc <chadrcollins91@gmail.com>
2016-12-22 22:14:38 -0500

    Hooked up article list items with onclick handler.

--- samples/simple-site/js/ArticlesPageController.js
+++ samples/simple-site/js/ArticlesPageController.js
@@ -3,52 +3,58 @@
  */
 
 class ArticlesPageController extends PageController {
-  constructor() {
-    super();
-    this.title = "Some Light Reading";
-    this.subTitle = "Enjoy";
+    constructor() {
+        super();
+        this.title = "Some Light Reading";
+        this.subTitle = "Enjoy";
 
-    this.data = ArticlesData;
+        this.data = ArticlesData;
 
-    this.getCatCount = this.getCatCount.bind(this);
-    this.getTagCount = this.getTagCount.bind(this);
-    this.getUntaggedCount = this.getUntaggedCount.bind(this);
-    this.catToggleChanged = this.catToggleChanged.bind(this);
-    this.tagToggleChanged = this.tagToggleChanged.bind(this);
-    this.allCatToggleChanged = this.allCatToggleChanged.bind(this);
-    this.allTagToggleChanged = this.allTagToggleChanged.bind(this);
-    this.untaggedToggleChanged = this.untaggedToggleChanged.bind(this);
-  }
+        this.getCatCount = this.getCatCount.bind(this);
+        this.getTagCount = this.getTagCount.bind(this);
+        this.getUntaggedCount = this.getUntaggedCount.bind(this);
+        this.catToggleChanged = this.catToggleChanged.bind(this);
+        this.tagToggleChanged = this.tagToggleChanged.bind(this);
+        this.allCatToggleChanged = this.allCatToggleChanged.bind(this);
+        this.allTagToggleChanged = this.allTagToggleChanged.bind(this);
+        this.untaggedToggleChanged = this.untaggedToggleChanged.bind(this);
+        this.articleItemClicked = this.articleItemClicked.bind(this);
+    }
 
-  getCatCount(boundContext) {
-    return this.data.catCounts[boundContext.index];
-  }
+    getCatCount(boundContext) {
+        return this.data.catCounts[boundContext.index];
+    }
 
-  getTagCount(boundContext) {
-    return this.data.tagCounts[boundContext.index];
-  }
+    getTagCount(boundContext) {
+        return this.data.tagCounts[boundContext.index];
+    }
 
-  getUntaggedCount(boundContext) {
-    return this.data.untaggedCount;
-  }
+    getUntaggedCount(boundContext) {
+        return this.data.untaggedCount;
+    }
 
-  catToggleChanged(event, boundContext) {
-    console.log(arguments);
-  }
+    catToggleChanged(event, boundContext) {
+        console.log(arguments);
+    }
 
-  allCatToggleChanged(event, boundContext) {
-    console.log(arguments);
-  }
+    allCatToggleChanged(event, boundContext) {
+        console.log(arguments);
+    }
 
-  tagToggleChanged(event, boundContext) {
-    console.log(arguments);
-  }
+    tagToggleChanged(event, boundContext) {
+        console.log(arguments);
+    }
 
-  allTagToggleChanged(event, boundContext) {
-    console.log(arguments);
-  }
+    allTagToggleChanged(event, boundContext) {
+        console.log(arguments);
+    }
 
-  untaggedToggleChanged(event, boundContext) {
-    console.log(arguments);
-  }
+    untaggedToggleChanged(event, boundContext) {
+        console.log(arguments);
+    }
+
+    articleItemClicked(event, boundContext) {
+        let article = this.data.articles[boundContext.index];
+        console.log(article);
+    }
 }
