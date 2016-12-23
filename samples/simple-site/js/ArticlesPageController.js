af4ce65fa4a9bfeaf3a2d50649e2548fc1c0dc80
chadrc <chadrcollins91@gmail.com>
2016-12-22 23:43:49 -0500

    Began implemented filters for article page in simple site.

--- samples/simple-site/js/ArticlesPageController.js
+++ samples/simple-site/js/ArticlesPageController.js
@@ -10,6 +10,14 @@ class ArticlesPageController extends PageController {
 
         this.data = ArticlesData;
 
+        this.activeCatFilters = [];
+        this.activeTagFilters = [];
+        this.anyCat = false;
+        this.anyTag = false;
+        this.untaggedFilter = false;
+
+        this.filteredArticles = this.data.articles;
+
         this.getCatCount = this.getCatCount.bind(this);
         this.getTagCount = this.getTagCount.bind(this);
         this.getUntaggedCount = this.getUntaggedCount.bind(this);
@@ -34,23 +42,38 @@ class ArticlesPageController extends PageController {
     }
 
     catToggleChanged(event, boundContext) {
-        console.log(arguments);
+        if (event.srcElement.checked) {
+            this.activeCatFilters.push(boundContext.value);
+        } else {
+            let index = this.activeCatFilters.indexOf(boundContext.value);
+            this.activeCatFilters.splice(index, 1);
+        }
+        this.filterArticles();
     }
 
     allCatToggleChanged(event, boundContext) {
-        console.log(arguments);
+        this.anyCat = event.srcElement.checked;
+        this.filterArticles();
     }
 
     tagToggleChanged(event, boundContext) {
-        console.log(arguments);
+        if (event.srcElement.checked) {
+            this.activeTagFilters.push(boundContext.value);
+        } else {
+            let index = this.activeTagFilters.indexOf(boundContext.value);
+            this.activeTagFilters.splice(index, 1);
+        }
+        this.filterArticles();
     }
 
     allTagToggleChanged(event, boundContext) {
-        console.log(arguments);
+        this.anyTag = event.srcElement.checked;
+        this.filterArticles();
     }
 
     untaggedToggleChanged(event, boundContext) {
-        console.log(arguments);
+        this.untaggedFilter = event.srcElement.checked;
+        this.filterArticles();
     }
 
     articleItemClicked(event, boundContext) {
@@ -59,5 +82,47 @@ class ArticlesPageController extends PageController {
         let modal = this.subContext.boundElement.subContexts.articleModal;
         modal.renderWithContext(article);
         $(modal.domElement).modal("show");
+    }
+
+    filterArticles() {
+        this.filteredArticles = [];
+
+        // If there are filters set, find matches
+        if (this.activeCatFilters.length > 0 ||
+            this.activeTagFilters.length > 0 ||
+            this.untaggedFilter || this.anyCat || this.anyTag) {
+
+            console.log(this.data.articles);
+            for (let article of this.data.articles) {
+                let catInFilter = false;
+                if (this.activeCatFilters.indexOf(article.category) >= 0) {
+                    catInFilter = true;
+                }
+                let catMatch = this.anyCat || catInFilter;
+
+                let tagInFilter = false;
+                if (article.tags.length == 0) {
+                    tagInFilter = this.untaggedFilter;
+                } else {
+                    for (let tag of article.tags) {
+                        if (this.activeTagFilters.indexOf(tag) >= 0) {
+                            tagInFilter = true;
+                            break;
+                        }
+                    }
+                }
+
+                let tagMatch = this.anyTag || tagInFilter;
+
+                if (catMatch && tagMatch) {
+                    this.filteredArticles.push(article);
+                }
+            }
+        } else {
+            // No filters set, show all articles
+            this.filteredArticles = this.data.articles;
+        }
+
+        this.subContext.renderWithContext(this);
     }
 }
